3180d658fba8a5217b3a3149b6973b16f41bc5f3
Yeseo Kim <YKim@lcs.on.ca>
2021-02-12 11:10:33 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Yeseo Kim on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
